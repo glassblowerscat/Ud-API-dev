0f985a47bb451db304606fb71ba212c13f93bbac
glassblowerscat <glassblower.random@gmail.com>
2018-10-24 14:27:50 -0400

    Require and Import Completed

---
+++ javascript-basics/exports.js
@@ -0,0 +1,18 @@
+export let myVar = 942;
+export let func1 = () => {
+  console.log('Hello from func1');
+};
+
+export let func2 = () => {
+  console.log('Hello from func2');
+};
+
+/* module.exports.myVar = myVariable;
+module.exports.func1 = func1;
+module.exports.func2 = func2; */
+
+/* module.exports = {
+  myVar: myVariable,
+  func1: func1,
+  func2: func2,
+} */
---
+++ javascript-basics/requireAndImport.js
@@ -0,0 +1,10 @@
+// let myFuncs = require('./exports');
+import { myVar, func1, func2 } from './exports';
+
+/* console.log(`The value of myVar is ${myFuncs.myVar}`);
+myFuncs.func1();
+myFuncs.func2(); */
+
+console.log(`The value of myVar is ${myVar}.`);
+func1();
+func2();
