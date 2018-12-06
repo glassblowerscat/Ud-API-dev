fa094ce63a43580afa26218e2a9fefcd03fe0ed6
Ryan Elainska <glassblowerscat@Ryans-MacBook-Pro.local>
2018-12-06 10:29:57 -0500

    Get all, get by ID, and create new restaurant added to controller

--- restaurant-api/src/routes/index.js
+++ restaurant-api/src/routes/index.js
@@ -3,6 +3,7 @@ import config from '../config';
 import middleware from '../middleware';
 import initializeDb from '../db';
 import app from '..';
+import restaurant from '../controller/restaurant';
 
 let router = express();
 
@@ -11,7 +12,9 @@ initializeDb(db => {
 
     // internal middleware
     router.use(middleware({ config, db }));
+    
     // api routes v1 (/v1)
+    router.use('/restaurant', restaurant({ config, db }));
 
 });
 
